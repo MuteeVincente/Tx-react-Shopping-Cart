61adbae0708d98636c9641ba5378df9a8270622d
MuteeVincente <vinniemutee@gmail.com>
2023-08-29 05:40:35 +0300

    currency-formatting

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
     id: number,
@@ -9,7 +10,17 @@ type StoreItemProps = {
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
     // Component logic goes here
     return <Card>
-        <Card.Img variant="top" src={imgUrl} height = "200px" style={{objectFit:'cover'}}></Card.Img>
+        <Card.Img variant="top" src={imgUrl} height = "200px" style={{objectFit:'cover'}}/>
+
+        <Card.Body className="d-flex flex-column">
+            <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                <span className="fs-2">{name}</span>
+                <span className="ms-2 text-muted">{formatCurrency (price)}</span>
+
+            </Card.Title>
+
+        </Card.Body>
+
     </Card>
 
 }
