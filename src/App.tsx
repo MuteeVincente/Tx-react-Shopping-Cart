c38f62505018255c16efb68704274432aaa04a37
MuteeVincente <vinniemutee@gmail.com>
2023-08-29 04:04:31 +0300

    Setting up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { useState } from 'react'
+import {Routes, Route} from  'react-router-dom';
+import {Container}  from 'react-bootstrap';
+import {Store}  from './pages/Store';
+import {Home} from './pages/Home';
+import {About} from './pages/About';
 
 
 function App() {
-  return <h1>Hi</h1>
+  return <Container>
+    <Routes>
+      <Route path= '/' element = {<Home/>}></Route>
+      <Route path= '/store' element = {<Store/>}></Route>
+      <Route path= '/about' element = {<About/>}></Route>
+    </Routes>
+
+  </Container>
 }
   
 export default App
