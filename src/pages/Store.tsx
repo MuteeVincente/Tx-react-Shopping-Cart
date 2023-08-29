399224cb03fb3042a10b92803a262484a9401bbe
MuteeVincente <vinniemutee@gmail.com>
2023-08-29 04:58:15 +0300

    code for store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,17 @@
+import { Col, Row } from 'react-bootstrap';
+import storeItems from '../data/items.json';
+
+
 export function  Store(){
-    return <h1>Store</h1>
+    return <><h1>Store</h1>
+    <Row>
+        {storeItems.map(item => (
+                        <Col>
+                        {JSON.stringify(item)}
+                        </Col>
+        ))}
+
+    </Row>
+
+</>
 }
