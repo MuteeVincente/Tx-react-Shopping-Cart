74b32177a263395e7e18df411dd4ad14473866cb
MuteeVincente <vinniemutee@gmail.com>
2023-08-30 05:00:35 +0300

    formatting currency for the cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import StoreItems from '../data/items.json';
 import { Stack } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type CartItemProps = {
     id: number
@@ -13,8 +14,23 @@ export function CartItem({id,quantity} : CartItemProps){
     if(item == null) return null
 
     return (
-        <Stack direction="horizontal" gap={2}>
+        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
             <img src = {item.imgUrl} style = {{width: "125px" , height: "75px" , objectFit: "cover"}}/>
+
+            <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+                </div>
+
+                <div className="text-muted" style={{fontSize: ".75rem"}}>
+                {formatCurrency(item.price)}
+                </div>
+            </div>
         </Stack>
 
     )
