cf0e9e6d3a34a4b531c001e9b8d5a811dcd751e4
MuteeVincente <vinniemutee@gmail.com>
2023-08-30 05:37:16 +0300

    Adding Json data

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,7 +4,7 @@ import {StoreItem}  from '../components/StoreItem';
 
 
 export function  Store(){
-    return <><h1>Store</h1>
+    return <>
     <Row md={2} xs ={1} lg={3} className='g-3'>
         {storeItems.map(item => (
                         <Col key ={item.id}>
